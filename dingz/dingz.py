d3cb5595b45fae8c1787bebdb5d7c07f6c01fa17
Fabian Affolter <mail@fabian-affolter.ch>
2020-01-10 23:53:32 +0100

    Add light sensor

--- dingz/dingz.py
+++ dingz/dingz.py
@@ -5,7 +5,7 @@ import aiohttp
 from yarl import URL
 
 from . import _request as request
-from .constants import API, DEVICE_INFO, TEMPERATURE
+from .constants import API, DEVICE_INFO, TEMPERATURE, LIGHT
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,22 +20,31 @@ class Dingz:
         self._session = session
         self._device_details = None
         self._temperature = None
+        self._intensity = None
+        self._day = None
         self.uri = URL.build(scheme="http", host=self._host).join(URL(API))
 
     async def get_device_info(self) -> None:
-        """Get the details from the switch."""
+        """Get the details from the dingz."""
         url = URL(self.uri).join(URL(DEVICE_INFO))
         response = await request(self, uri=url)
         self._device_details = response
 
     async def get_temperature(self) -> None:
-        """Get the details from the switch."""
+        """Get the room temperature from the dingz."""
         url = URL(self.uri).join(URL(TEMPERATURE))
         response = await request(self, uri=url)
         self._temperature = response["temperature"]
 
+    async def get_light(self) -> None:
+        """Get the light details from the switch."""
+        url = URL(self.uri).join(URL(LIGHT))
+        response = await request(self, uri=url)
+        self._intensity = response["intensity"]
+        self._day = response["day"]
+
     @property
-    def device_details(self) -> float:
+    def device_details(self) -> str:
         """Return the current device details."""
         return self._device_details
 
@@ -43,6 +52,16 @@ class Dingz:
     def temperature(self) -> float:
         """Return the current temperature in celsius."""
         return round(self._temperature, 1)
+
+    @property
+    def day(self) -> bool:
+        """Return true if the sensor thinks it's day."""
+        return bool(self._day)
+
+    @property
+    def intensity(self) -> float:
+        """Return the current light intensity in lux."""
+        return round(self._intensity, 1)
 
     async def close(self) -> None:
         """Close an open client session."""
