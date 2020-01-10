1425da242e1b6e4b4b289024551fb176f8713f78
Fabian Affolter <mail@fabian-affolter.ch>
2020-01-10 23:41:11 +0100

    Move constants

--- dingz/constants.py
+++ dingz/constants.py
@@ -1,29 +1,34 @@
-"""Constants used by the Python API for interacting with Dingz devices."""
+"""Constants used by the Python API for interacting with dingz units."""
+import pkg_resources
 
-API = '/api/v1/'
+try:
+    __version__ = pkg_resources.get_distribution("setuptools").version
+except Exception:
+    __version__ = "unknown"
+
+TIMEOUT = 10
+
+USER_AGENT = f"PythonDingz/{__version__}"
+API = "/api/v1/"
 
 # Status endpoints
-PUCK = 'f{API}puck'
-INFO = 'f{API}info'
-TEMPERATURE = 'f{API}temp'
-DIMMER = 'f{API}dimmer'
-MOTION = 'f{API}/motion'
-DEVICE_INFO = 'f{API}device'
-LIGHT = 'f{API}light'
+PUCK = "puck"
+INFO = "info"
+TEMPERATURE = "temp"
+DIMMER = "dimmer"
+MOTION = "motion"
+DEVICE_INFO = "device"
+LIGHT = "light"
 
 # Special endpoints
-LOG = '/log'
-FIRMWARE = '/load'
+LOG = "/log"
+FIRMWARE = "/load"
 
-
-
-SETTINGS = 'f{API}settings'
-FRONT_LED = 'f{API}led/get'
-BUTTON_ACTIONS = 'f{API}action'
-
+SETTINGS = "settings"
+FRONT_LED = "led/get"
+BUTTON_ACTIONS = "action"
 
 # Configuration endpoints
-PIR_CONFIGURATION = 'f{API}pir_config'
-THERMOSTAT_CONFIGURATION = 'f{API}thermostat_config'
-INPUT_CONFIGURATION = 'f{API}input_config'
-
+PIR_CONFIGURATION = "pir_config"
+THERMOSTAT_CONFIGURATION = "thermostat_config"
+INPUT_CONFIGURATION = "input_config"
