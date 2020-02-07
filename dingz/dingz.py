c667c7f639efacac6e7818d37a24ed91016f39fc
Fabian Affolter <mail@fabian-affolter.ch>
2020-02-07 18:25:31 +0100

    Add configurations

--- dingz/dingz.py
+++ dingz/dingz.py
@@ -5,7 +5,7 @@ import aiohttp
 from yarl import URL
 
 from . import make_call
-from .constants import API, DEVICE_INFO, TEMPERATURE, LIGHT, FRONT_LED_GET, FRONT_LED_SET, PUCK, SETTINGS
+from .constants import API, DEVICE_INFO, TEMPERATURE, LIGHT, FRONT_LED_GET, FRONT_LED_SET, PUCK, SETTINGS, PIR_CONFIGURATION, THERMOSTAT_CONFIGURATION, INPUT_CONFIGURATION
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -33,7 +33,7 @@ class Dingz:
 
     async def get_info(self) -> None:
         """Get everything from the dingz unit."""
-        for endpoint in [PUCK, DEVICE_INFO, SETTINGS]:
+        for endpoint in [PUCK, DEVICE_INFO, SETTINGS, PIR_CONFIGURATION, THERMOSTAT_CONFIGURATION, INPUT_CONFIGURATION]:
             url = URL(self.uri).join(URL(endpoint))
             self._catch_all[endpoint] = await make_call(self, uri=url)
 
