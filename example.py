09a9cce2209b4f1bd29b4ac4974f3dd505305ffb
Simon Berger <simon@siku2.io>
2025-08-10 16:20:13 +0000

    fix: DeprecationWarning: There is no current event loop

--- example.py
+++ example.py
@@ -63,5 +63,4 @@ async def main():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
