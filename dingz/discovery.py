eaafcf919f0b3f8c452e80c16ca67fd3a75b74ae
Simon Berger <simon@siku2.io>
2025-08-11 17:19:55 +0000

    feat: update discovery to use zeroconf

--- dingz/discovery.py
+++ dingz/discovery.py
@@ -1,119 +1,240 @@
-"""Discover dingz devices in a network."""
+"""Dingz discovery.
+
+The discovery is based on mDNS.
+dingz devices announce a HTTP local TXT service on port 80 over mDNS. Hostname is always in
+the form of `DINGZ-<model>-<MAC>`.
+Eg.: `DINGZ-dz1f-4b-30C6F72355C4.local TXT`
+"""
 
 from __future__ import annotations
 
 import asyncio
+import dataclasses
 import logging
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
+from typing import TYPE_CHECKING
 
-from .constants import DEVICE_MAPPING
+from typing_extensions import Self
+from zeroconf import ServiceInfo, ServiceListener, Zeroconf
+from zeroconf.asyncio import (
+    AsyncServiceBrowser,
+    AsyncServiceInfo,
+    AsyncZeroconf,
+)
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator, Callable, Mapping
+    from types import TracebackType
+
+__all__ = [
+    "Discovery",
+    "Properties",
+    "discover",
+    "discover_list",
+]
 
 _LOGGER = logging.getLogger(__name__)
 
+_DINGZ_NAME_PREFIX = "DINGZ "
+_DEFAULT_HTTP_PORT = 80
 
-class DiscoveredDevice:
-    """Representation of discovered device."""
 
+@dataclasses.dataclass(frozen=True)
+class Properties:
+    """Properties of a discovered dingz device."""
+
+    fw_version: str
+    """The firmware version of the device."""
+    protected: bool
+    """Whether the device is protected."""
+    pir: bool
+    """Whether the device has a PIR."""
+    id: str
+    """Dingz ID.
+
+    This is different from the serial number and can be set by the user.
+    """
+    home_name: str
+    """User provided home name."""
+    room_name: str
+    """User provided room name."""
+    device_name: str
+    """User provided device name."""
     mac: str
-    type: int
-    is_child: bool
-    mystrom_registered: bool
-    mystrom_online: bool
-    restarted: bool
+    """Mac address of the device."""
 
-    @staticmethod
-    def create_from_announce_msg(raw_addr, announce_msg):
-        """Create announce message."""
-        _LOGGER.debug("Received announce message '%s' from %s ", announce_msg, raw_addr)
-        if len(announce_msg) != 8:
-            msg = f"Unexpected announcement, '{announce_msg}'"
-            raise RuntimeError(msg)
-
-        device = DiscoveredDevice(host=raw_addr[0], mac=announce_msg[0:6].hex(":"))
-        device.type = announce_msg[6]
-        try:
-            device.hardware = DEVICE_MAPPING[str(announce_msg[6])]
-        except KeyError:
-            device.hardware = "unknown"
-        status = announce_msg[7]
-
-        # Parse status field
-        device.is_child = status & 1 != 0
-        device.mystrom_registered = status & 2 != 0
-        device.mystrom_online = status & 4 != 0
-        device.restarted = status & 8 != 0
-        return device
-
-    def __init__(self, host, mac) -> None:
-        """Initialize the discovery."""
-        self.host = host
-        self.mac = mac
-
-
-class DeviceRegistry:
-    """Representation of the device registry."""
-
-    def __init__(self) -> None:
-        """Initialize the device registry."""
-        self.devices_by_mac = {}
-
-    def register(self, device) -> None:
-        """Register a device."""
-        self.devices_by_mac[device.mac] = device
-
-    def devices(self):
-        """Get all present devices."""
-        return list(self.devices_by_mac.values())
-
-
-class DiscoveryProtocol(asyncio.DatagramProtocol):
-    """Representation of the discovery protocol."""
-
-    def __init__(self, registry: DeviceRegistry) -> None:
-        """ "Initialize the discovery protocol."""
-        super().__init__()
-        self.registry = registry
-
-    def connection_made(self, transport) -> None:
-        """Create an UDP listener."""
-        _LOGGER.debug("Starting up UDP listener")
-        self.transport = transport
-
-    def datagram_received(self, data, addr) -> None:
-        """Handle a datagram."""
-        device = DiscoveredDevice.create_from_announce_msg(addr, data)
-        self.registry.register(device)
-
-    def connection_lost(self, exc: Exception | None) -> None:
-        """Stop if connection is lost."""
-        _LOGGER.debug("Shutting down UDP listener")
-        super().connection_lost(exc)
-
-
-async def discover_dingz_devices(timeout: int = 7) -> list[DiscoveredDevice]:
-    """
-    Try to discover all local dingz instances. All dingz instances
-    report their presence every ~5 seconds in an UDP broadcast to port 7979.
-
-    :param timeout: timeout in seconds for discover.
-    :return: list of discovered devices
-    """
-    registry = DeviceRegistry()
-    loop = asyncio.get_event_loop()
-    (transport, protocol) = await loop.create_datagram_endpoint(
-        lambda: DiscoveryProtocol(registry), local_addr=("0.0.0.0", 7979)
-    )
-    # Server runs in the background, meanwhile wait until timeout expires
-    await asyncio.sleep(timeout)
-
-    # Shutdown server
-    transport.close()
-
-    devices = registry.devices()
-    for device in devices:
-        _LOGGER.debug(
-            "Discovered dingz %s (%s) (MAC address: %s)",
-            device.host,
-            device.type,
-            device.mac,
+    @classmethod
+    def from_zeroconf_properties(cls, props: Mapping[str, str | None]) -> Self:
+        """Read the properties from a zeroconf discovery."""
+        return cls(
+            fw_version=str(props["fw"]),
+            protected=props["protected"] == "true",
+            pir=props["pir"] == "true",
+            id=str(props["id"]),
+            home_name=str(props["home"]),
+            room_name=str(props["room"]),
+            device_name=str(props["name"]),
+            mac=str(props["mac"]),
         )
-    return devices
+
+
+@dataclasses.dataclass(frozen=True)
+class Discovery:
+    """A discovered dingz device."""
+
+    host: str
+    """Host address of the device."""
+    props: Properties
+    """Properties of the discovered device."""
+
+    @classmethod
+    def from_service_info(cls, info: ServiceInfo) -> Self:
+        """Create a Discovery instance from a ServiceInfo instance."""
+        props = Properties.from_zeroconf_properties(info.decoded_properties)
+        addresses = info.parsed_addresses()
+        if not addresses:
+            msg = "ServiceInfo has no parsed addresses"
+            raise ValueError(msg)
+        host = addresses[0]
+        if info.port != _DEFAULT_HTTP_PORT:
+            host = f"{host}:{info.port}"
+
+        return cls(host=host, props=props)
+
+
+async def discover_list(duration: float = 10.0) -> list[Discovery]:
+    """Discover dingz devices on the network.
+
+    This is a simpler version of the more flexible `discover` function.
+
+    Args:
+        duration: The duration to listen for devices (in seconds).
+
+    Returns:
+        A list of discovered dingz devices.
+    """
+    discoveries: list[Discovery] = []
+
+    def callback(discovery: Discovery) -> None:
+        discoveries.append(discovery)
+
+    async with _discover_with_callback(callback, timeout=duration) as _:
+        await asyncio.sleep(duration)
+    return discoveries
+
+
+def discover(
+    *,
+    duration: float | None = None,
+    timeout: float = 10.0,
+) -> AbstractAsyncContextManager[AsyncIterator[Discovery]]:
+    """Discover dingz devices on the network.
+
+    This function is an async context manager. Entering it starts the discovery process and returns
+    an async iterator which yields the discoveries as they are made.
+
+    Consider using the `discover_list` function with a simpler interface if you don't need to get
+    discoveries in real-time.
+
+    Args:
+        duration: The time in seconds to perform discovery. `None` means forever (or until the
+                  context manager is exited). Any pending discoveries will still be yielded.
+        timeout: Timeout for requesting service info from a discovered device. This is an advanced
+                 parameter and likely doesn't need to be changed.
+
+    Example:
+        async with discover() as discoveries:
+            async for discovery in discoveries:
+                print(discovery)
+    """
+    loop = asyncio.get_running_loop()
+    queue: asyncio.Queue[Discovery | None] = asyncio.Queue()
+
+    async def reader() -> AsyncIterator[Discovery]:
+        while True:
+            item = await queue.get()
+            # 'None' signals the end of discovery
+            if item is None:
+                break
+            yield item
+
+    @asynccontextmanager
+    async def _discover() -> AsyncIterator[AsyncIterator[Discovery]]:
+        handle: asyncio.TimerHandle | None = None
+        if duration is not None:
+            # Add 'None' to the queue to signal the end of discovery
+            handle = loop.call_later(duration, queue.put_nowait, None)
+
+        try:
+            async with _discover_with_callback(queue.put_nowait, timeout=timeout):
+                yield reader()
+        finally:
+            # Clean up timer handle if we started one
+            if handle is not None:
+                handle.cancel()
+
+    return _discover()
+
+
+class _Listener(ServiceListener):
+    def __init__(self, callback: Callable[[Discovery], None], timeout: float) -> None:
+        super().__init__()
+        self._loop = asyncio.get_running_loop()
+        self._timeout_ms = 1000 * timeout
+        self._callback = callback
+        self._tasks: list[asyncio.Task[None]] = []
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        try:
+            for task in self._tasks:
+                await task
+            self._tasks.clear()
+        finally:
+            # Any tasks still pending (which could happen if we get here because of an exception)
+            # are cancelled.
+            for task in self._tasks:
+                task.cancel()
+
+    def add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        if not name.startswith(_DINGZ_NAME_PREFIX):
+            return
+        task = self._loop.create_task(self._add_service(zc, type_, name))
+        self._tasks.append(task)
+
+    async def _add_service(self, zc: Zeroconf, type_: str, name: str) -> None:
+        info = AsyncServiceInfo(type_, name)
+        await info.async_request(zc, self._timeout_ms)
+        if not info:
+            _LOGGER.debug("Failed to discover %s", name)
+            return
+        discovery = Discovery.from_service_info(info)
+        _LOGGER.debug("Discovered %s (%s)", discovery, info)
+        self._callback(discovery)
+
+
+@asynccontextmanager
+async def _discover_with_callback(
+    callback: Callable[[Discovery], None],
+    *,
+    timeout: float,
+) -> AsyncIterator[None]:
+    """Discover dingz devices with a callback.
+
+    This function is a async context manager. While entered, it will handle zeroconf announcements
+    and call the provided function with every device discovered. The callback will not be called
+    again as soon as as the context manager is exited.
+    """
+    async with (
+        AsyncZeroconf() as zc,
+        _Listener(callback, timeout=timeout) as listener,
+        AsyncServiceBrowser(zc.zeroconf, "_http._tcp.local.", listener=listener),
+    ):
+        yield
